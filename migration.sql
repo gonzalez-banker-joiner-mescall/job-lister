6e24d5c2aebfbd62f520eb8c345686de4208951d
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-05-09 12:39:28 -0400

    migration refactored to create db

--- migration.sql
+++ migration.sql
@@ -1,22 +1,51 @@
-USE adlister_db;
+CREATE database IF NOT EXISTS joblister_db;
 
-DROP TABLE IF EXISTS ads;
-DROP TABLE IF EXISTS users;
+USE joblister_db;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username VARCHAR(240) NOT NULL,
-    email VARCHAR(240) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+DROP TABLE IF EXISTS `ad_cat`;
+DROP TABLE IF EXISTS `ads`;
+DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `categories`
+(
+    `id`   BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255)    NOT NULL
+);
+ALTER TABLE
+    `categories`
+    ADD UNIQUE `categories_name_unique` (`name`);
+CREATE TABLE `ad_cat`
+(
+    `ad_id`  BIGINT UNSIGNED NOT NULL,
+    `cat_id` BIGINT UNSIGNED NOT NULL
+);
+CREATE TABLE `users`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255)    NOT NULL,
+    `password` VARCHAR(255)    NOT NULL,
+    `villain`  BOOLEAN         NOT NULL
 );
 
-CREATE TABLE ads (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id INT UNSIGNED NOT NULL,
-    title VARCHAR(240) NOT NULL,
-    description TEXT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
-        ON DELETE CASCADE
+ALTER TABLE
+    `users`
+    ADD UNIQUE `users_username_unique` (`username`);
+CREATE TABLE `ads`
+(
+    `id`          BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title`       TEXT            NOT NULL,
+    `description` TEXT            NOT NULL,
+    `created`     DATETIME        NOT NULL,
+    `user_id`     BIGINT UNSIGNED NOT NULL
 );
+
+ALTER TABLE
+    `ad_cat`
+    ADD CONSTRAINT `ad_cat_cat_id_foreign` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`);
+ALTER TABLE
+    `ad_cat`
+    ADD CONSTRAINT `ad_cat_ad_id_foreign` FOREIGN KEY (`ad_id`) REFERENCES `ads` (`id`);
+ALTER TABLE
+    `ads`
+    ADD CONSTRAINT `ads_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
