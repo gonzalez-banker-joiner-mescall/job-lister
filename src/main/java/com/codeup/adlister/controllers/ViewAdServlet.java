6f403d881a2dedc4974f4399b95a3da7807dce68
thomas-mescall <122935621+thomas-mescall@users.noreply.github.com>
2023-05-11 14:45:01 -0500

    completed connection of ad-list to ad-indiv-page

--- src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
@@ -1,6 +1,9 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.dao.MySQLAdsDao;
+import com.codeup.adlister.models.Ad;
+import com.mysql.cj.Session;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -11,13 +14,11 @@ import java.io.IOException;
 public class ViewAdServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getParameter("adId");
+
+        Long adId = Long.valueOf(request.getParameter("adId"));
+        Ad ad = DaoFactory.getAdsDao().findAdById(adId);
+        request.setAttribute("ad", ad);
 
         request.getRequestDispatcher("/WEB-INF/ads/view-ad.jsp").forward(request, response);
     }
-
-//    @Override
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
 }
