30432b4f9ab5d55010e5be9108b33ba9a08473ac
Austin Joiner <austin.j.joiner1@gmail.com>
2023-05-12 16:26:52 -0400

    added minion links

--- src/main/webapp/js/landingpage.js
+++ src/main/webapp/js/landingpage.js
@@ -15,9 +15,9 @@ const observer = new MutationObserver((mutationsList) => {
             const style = target.getAttribute('style');
             if (style.includes('left:') && parseFloat(style.split('left:')[1]) < 50) {
                 // Do something here
-                document.getElementById("minion-bubbles").textContent = "Well it can get crazy around here. If you are a minion accept and job and if you are an admin create them. Sign up or log in.";
+                document.getElementById("minion-bubbles").innerHTML = `Well it can get crazy around here. If you are a minion accept and job and if you are an admin create them. <span><a href='/register'>Sign up</a></span> or <span><a href='/login'>Login</a></span>.`;
             } else {
-                document.getElementById("minion-bubbles").textContent = "Hello I am Minion #24601 I am your AI minion helper. This site is for minions to accept jobs from your supreme leader, Gru and senior level minions.";
+                document.getElementById("minion-bubbles").innerHTML = `Hello I am Minion #24601 I am your AI minion helper. This site is for minions to accept jobs from your supreme leader, Gru and senior level minions.`;
             }
         }
     }
